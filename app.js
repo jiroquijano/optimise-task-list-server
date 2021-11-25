ed9aa532d525f2a773eef709907fab3dcfa09830
safiro.quijano <safiro.quijano@paymaya.com>
2021-11-26 06:31:45 +0800

    added cors module

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 const listRouter = require('./routers/listRouter');
 const taskRouter = require('./routers/taskRouter');
+var cors = require('cors');
+
 
 app.use(express.json());
+app.use(cors());
 app.use(listRouter);
 app.use(taskRouter);
 
